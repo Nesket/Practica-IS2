1dd25c283dbf40b3a6d8cd4afbd574f53e5db5f3
ruizdiasmatt <m_d_125@hotmail.com>
2024-10-05 14:28:55 +0200

    Static Error Solved (Adaptability major) at BLFacadeImplementation.java

--- src/main/java/businessLogic/BLFacadeImplementation.java
+++ src/main/java/businessLogic/BLFacadeImplementation.java
@@ -21,16 +21,17 @@ import domain.Complaint;
 import domain.Movement;
 import exceptions.RideMustBeLaterThanTodayException;
 import exceptions.RideAlreadyExistException;
-
+import java.util.logging.Logger;
 /**
  * It implements the business logic as a web service.
  */
 @WebService(endpointInterface = "businessLogic.BLFacade")
 public class BLFacadeImplementation implements BLFacade {
 	DataAccess dbManager;
+	private static final Logger logger = Logger.getLogger(BLFacadeImplementation.class.getName());
 
 	public BLFacadeImplementation() {
-		System.out.println("Creating BLFacadeImplementation instance");
+		logger.info("Creating BLFacadeImplementation instance");
 
 		dbManager = new DataAccess();
 
@@ -40,7 +41,7 @@ public class BLFacadeImplementation implements BLFacade {
 
 	public BLFacadeImplementation(DataAccess da) {
 
-		System.out.println("Creating BLFacadeImplementation instance with DataAccess parameter");
+		logger.info("Creating BLFacadeImplementation instance with DataAccess parameter");
 		@SuppressWarnings("unused")
 		ConfigXML c = ConfigXML.getInstance();
 
