ba1dd307cf0bb327b09292dfd516fe8b7c7b954e
kintxaustiehu <kintxausti001@ikasle.ehu.eus>
2024-10-09 17:31:24 +0200

    Cambiar el uso de "==" por el uso de "equals" para evitar errores de sonarlint

--- src/main/java/domain/Driver.java
+++ src/main/java/domain/Driver.java
@@ -95,9 +95,7 @@ public class Driver extends User implements Serializable {
 		int index=0;
 		boolean encontrado=false;
 		for (Ride ride : createdRides) {
-			//if (ride.getFrom().equals(from) && ride.getTo().equals(to) && ride.getDate().equals(date)) {
-			if ((ride.getFrom()==from) && (ride.getTo()==to) && (ride.getDate().equals(date))) {
-
+			if (ride.getFrom().equals(from) && ride.getTo().equals(to) && ride.getDate().equals(date)){
 				encontrado=true;
 				pos=index;
 			}
