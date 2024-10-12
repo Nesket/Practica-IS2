0dd96f7418a2337ffcde9647ce96616596982f37
kintxaustiehu <kintxausti001@ikasle.ehu.eus>
2024-10-12 14:53:15 +0200

    Actualizar cómo se usa "erreklamazioaBidali" en BLFacadeImplementation

--- src/main/java/businessLogic/BLFacadeImplementation.java
+++ src/main/java/businessLogic/BLFacadeImplementation.java
@@ -8,6 +8,7 @@ import javax.jws.WebService;
 
 import configuration.ConfigXML;
 import dataAccess.DataAccess;
+import dataAccess.ErreklamazioaBidaliParameter;
 import domain.Ride;
 import domain.Traveler;
 import domain.User;
@@ -330,7 +331,7 @@ public class BLFacadeImplementation implements BLFacade {
 	@Override
 	public boolean erreklamazioaBidali(String nor, String nori, Date gaur, Booking book, String textua, boolean aurk) {
 		dbManager.open();
-		boolean sent = dbManager.erreklamazioaBidali(nor, nori, gaur, book, textua, aurk);
+		boolean sent = dbManager.erreklamazioaBidali(new ErreklamazioaBidaliParameter(nor, nori, gaur, book, textua, aurk));
 		dbManager.close();
 		return sent;
 	}
